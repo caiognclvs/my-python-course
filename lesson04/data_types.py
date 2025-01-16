d591a8c943ab45b2ea8576d38e9fd7e65ee9d8b7
caio <caiognclvs@proton.me>
2025-01-16 09:31:44 -0800

    first commit

---
+++ lesson04/data_types.py
@@ -0,0 +1,18 @@
+first = "Caio"
+last = "Goncalves"
+
+# print(type(first))
+# print(type(last) == str)
+# print(isinstance(first, str))
+
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+fullname = first + " " + last
+print(fullname)
+fullname += "!"
+print(fullname)
+
+decade = str(2000)
